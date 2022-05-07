090f5050c63225dfbaca81cc628124cf367c9a26
Abhay Kumar Ray <abhayray2002@gmail.com>
2022-05-07 12:40:38 +0530

    database connected

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+
+const indexRouter = require('./routes/index');
 
 app.set('view engine', 'ejs');
 app.set('views',__dirname + '/views');
@@ -8,6 +14,13 @@ app.set('layout','layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on('error', err => console.error(err));
+db.once('open',  () => console.log('Connected to database'));
+
+app.use('/',indexRouter);
 
 app.listen(process.env.PORT || 3000, function(){
     console.log('listening on port 3000');
